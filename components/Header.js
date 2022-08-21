a3116fab103663eb0e5a86cd6e858b6ff4c1e09d
Alexander Ruben Mamani <labm2014@gmail.com>
2022-08-21 18:49:54 -0300

    arreglando el menu modo escritorio

--- components/Header.js
+++ components/Header.js
@@ -19,18 +19,21 @@ const Header = () => {
                             <Image width={80} height={80} src='/img/logo/logo.jpeg' alt='logo de la compania'/>        
                     </Link>
                 </div>
+                <nav className={ `${styles.menuLink} ${ menu ? styles.active : null }`}>
+                    <Link href={'/'}>INICIO</Link>
+                    <Link href={'/productos'}>PRODUCTOS</Link>
+                    <Link href={'/acerca'}>ACERCA DE</Link>
+                    <Link href={'/contacto'}>CONTACTO</Link>
+                </nav>
                 
                 <nav className={styles.menuBars} onClick={handleClick}>
                     <Image width={45} height={45} src='/img/icon/bars-solid.svg' alt='menu'/>
                 </nav>
+             
             </div>    
         </div>
-        <nav className={ `${styles.menuLink} ${ menu ? styles.active : null }`}>
-            <Link href={'/'}>INICIO</Link>
-            <Link href={'/productos'}>PRODUCTOS</Link>
-            <Link href={'/acerca'}>ACERCA DE</Link>
-            <Link href={'/contacto'}>CONTACTO</Link>
-        </nav>
+        
+        
     </header>
   )
 }
