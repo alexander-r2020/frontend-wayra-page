40134d8a8ca33406f790e3fe6c4f1cef320454f1
Alexander Ruben Mamani <labm2014@gmail.com>
2022-08-21 13:32:51 -0300

    terminando el menu para mobile

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,15 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Header.module.css'
+import { useState } from 'react'
 const Header = () => {
+
+    const [menu,setMenu] = useState(false)
+
+    const handleClick=()=>{
+        setMenu(!menu)
+    }
+
   return (
     <header className={styles.header}>
         <div className="contenedor">
@@ -11,17 +19,18 @@ const Header = () => {
                             <Image width={80} height={80} src='/img/logo/logo.jpeg' alt='logo de la compania'/>        
                     </Link>
                 </div>
-                <nav className={styles.menuLink}>
-                    <Link href={'/'}>HOME</Link>
-                    <Link href={'/productos'}>PRODUCTOS</Link>
-                    <Link href={'/acerca'}>ACERCA DE</Link>
-                    <Link href={'/contacto'}>CONTACTO</Link>
-                </nav>
-                <nav className={styles.menuBars}>
+                
+                <nav className={styles.menuBars} onClick={handleClick}>
                     <Image width={45} height={45} src='/img/icon/bars-solid.svg' alt='menu'/>
                 </nav>
             </div>    
         </div>
+        <nav className={ `${styles.menuLink} ${ menu ? styles.active : null }`}>
+            <Link href={'/'}>INICIO</Link>
+            <Link href={'/productos'}>PRODUCTOS</Link>
+            <Link href={'/acerca'}>ACERCA DE</Link>
+            <Link href={'/contacto'}>CONTACTO</Link>
+        </nav>
     </header>
   )
 }
